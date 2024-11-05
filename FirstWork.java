75155ceea366e6121fa53d2465d76a3a45f381e1
Sunny <yangchingwen30@gmail.com>
2024-11-06 01:29:48 +0800

    commit again

--- FirstWork.java
+++ FirstWork.java
@@ -1,5 +1,6 @@
 public class FirstWork {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Master branch modification");
     }
 }
